52d687510cfeb8b21d6ffdf6ef4c6bde5b81a8e5
aaryan200588 <aaryankale.008@gmail.com>
2026-01-18 14:52:26 +0530

    Phase 2 Complete:admin routes protected with JWT

---
+++ server/middleware/adminAuthMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import Admin from "../models/Admin.js";
+
+const adminProtect = async (req, res, next) => {
+  let token;
+
+  // Check Authorization header
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Extract token
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get admin from DB
+      req.admin = await Admin.findById(decoded.id).select("-password");
+
+      next(); // allow request
+    } catch (error) {
+      return res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  } else {
+    return res.status(401).json({ message: "Not authorized, no token" });
+  }
+};
+
+export default adminProtect;
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,8 +1,18 @@
 import express from "express";
 import { adminLogin } from "../controllers/adminController.js";
+import adminProtect from "../middleware/adminAuthMiddleware.js";
 
 const router = express.Router();
 
+// PUBLIC ROUTE
 router.post("/login", adminLogin);
+
+// 🔒 PROTECTED ROUTE
+router.get("/dashboard", adminProtect, (req, res) => {
+  res.json({
+    message: "Welcome Admin",
+    admin: req.admin,
+  });
+});
 
 export default router;

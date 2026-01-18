f8673fa4d609c9151d2a3d67f9e4eaa6f41bc9c4
aaryan200588 <aaryankale.008@gmail.com>
2026-01-18 14:34:39 +0530

    Cleanup: remove temporary admin creation code

--- server/index.js
+++ server/index.js
@@ -2,23 +2,31 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+
+// ROUTES
 import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
 
 dotenv.config();
 connectDB();
 
-
 const app = express();
+
+// GLOBAL MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
+// ROUTES
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+
+// START SERVER
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
-
-app.use("/api/auth", authRoutes);

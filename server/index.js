be22cdd6aa1ed9285e962e7ddaef0062e8663df9
shravaseven <shravankhatavkar7@gmail.com>
2026-01-18 08:37:12 +0530

    Add auth routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -19,3 +20,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+
+app.use("/api/auth", authRoutes);
